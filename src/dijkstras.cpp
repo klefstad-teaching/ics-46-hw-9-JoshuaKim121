91f75d7803e91d4e355251712b64885762597ceb
JoshuaKim121 <joshuakim121@gmail.com>
2025-03-15 19:57:15 +0000

    Defined extract_shortest_path and print_path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -34,3 +34,26 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 
     return distance;
 }
+
+
+vector<int> extract_shortest_path(const vector<int>& distance, const vector<int>& previous, int destination) {
+    vector<int> path;
+
+    for (int v = destination; v != -1; v = previous[v]) {
+        path.push_back(v);
+    }
+    reverse(path.begin(), path.end());
+
+    return path;
+}
+
+
+void print_path(const vector<int>& path, int total) {
+    if (path.empty()) return;
+
+    cout << "Shortest path: ";
+    for (int vertex : path) {
+        cout << vertex << " ";
+    }
+    cout << "\nTotal distance: " << total << endl;
+}
