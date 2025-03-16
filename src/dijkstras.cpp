86876a927cecea2b10abffbd693b05f99702c1a4
JoshuaKim121 <joshuakim121@gmail.com>
2025-03-16 20:03:36 +0000

    Fixed gtest build error and print typo

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -4,24 +4,32 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     int numVertices = G.size();
     vector<int> distances(numVertices, INF);
     vector<bool> visited(numVertices, false);
+    
     distances[source] = 0;
     previous[source] = -1;
-    priority_queue<pair<int, int>> minHeap;
-    minHeap.push({source, 0});
-    while(!minHeap.empty()) {
-        int u = minHeap.extractVertexWithMinimumWeight().first;
-        if visited[u] continue;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+    minHeap.push({0, source});
+
+    while (!minHeap.empty()) {
+        int u = minHeap.top().second;
+        minHeap.pop();
+
+        if (visited[u]) continue;
         visited[u] = true;
-        for(Edge edge: graph[u]) {
-            int v = edge.dest;
-            int weight = edge.second;
+
+        for (const Edge& edge : G[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
+
             if (!visited[v] && distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                minHeap.push({v, distances[v]});
+                minHeap.push({distances[v], v});
             }
         }
     }
+
     return distances;
 }
 
